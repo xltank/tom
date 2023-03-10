8f8ab4448c8272a0496d5e22016e5342b352dbd9
徐立 <xuli@uniwyse.com>
2023-03-10 17:56:26 +0800

    codes

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package tom
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/xltank/tom/logger"
 	"github.com/xltank/tom/model"
 )
 
@@ -18,8 +18,17 @@ type Tom struct {
 
 func (s *Tom) Listen(address string) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Request in, ", r.URL)
-		w.Write([]byte("Success"))
+		logger.Log("Request in, r.URL:", r.URL)
+
+		for i := 0; i < len(s.Routes); i++ {
+			route := s.Routes[i]
+			logger.Log("finding route:", r.Method, r.URL.Path, ",", route.Method, route.Path)
+			if r.Method == route.Method && r.URL.Path == route.Path {
+				route.Handles[0](w, r)
+			}
+		}
+
+		// w.Write([]byte("Success"))
 	})
 	http.ListenAndServe(address, nil)
 }
